ab63def28d7fbb2cfff47eeaa945c910b4d8ef0a
ngiann <ngiann@gmail.com>
2023-11-14 23:24:09 +0100

    train with original targets

--- src/distill.jl
+++ src/distill.jl
@@ -16,29 +16,18 @@ function distill(X, C, W, Z, scales; iterations = 1, α = 0.1)
     @printf("\t number of BLAS threads is %d\n", BLAS.get_num_threads())
     @printf("\t number of julia threads is %d\n", Threads.nthreads())
 
-    # define ensemble output
+    # define network
 
     t(x) = softmax(x, dims=1)
 
-    f = net(Q = Q, M = M, D = D, o = t)
-    
-    
-    # calculate targets
-
-    T = let
-
-        F(W, z) = mapreduce(w -> f(w, z), +, W) / K
-    
-        F(W, Z)*Diagonal(scales)
-        
-    end
-    
+    f = net(Q = Q, M = M, D = D, o = t)    
+  
 
     #-------------------------------------------
     function objective(w)
     #-------------------------------------------
 
-       sum((abs2.(T - f(w, Z)*Diagonal(scales)) ./ C)) + α*sum(abs2.(w))
+       sum((abs2.(X - f(w, Z)*Diagonal(scales)) ./ C)) + α*sum(abs2.(w))
 
     end
 
