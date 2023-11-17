06591d848459b5a0c8a984e2b20edee00029d69a
ngiann <ngiann@gmail.com>
2023-11-17 14:27:58 +0100

    back to old version of net - need to investigate

--- src/net.jl
+++ src/net.jl
@@ -11,7 +11,7 @@ function net(; Q::Int = Q, M::Int = M, D::Int = D, o = NNlib.softmax)
 
 end
 
-numparam(n::net) = (n.M*n.Q + n.M) + (n.D*n.M)
+numparam(n::net) = (n.M*n.Q + n.M) + (n.D*n.M + n.D)
 
 
 function unpack(n::net, param)
@@ -30,6 +30,10 @@ function unpack(n::net, param)
 
     MARK += M*Q
 
+    b2 = @view param[MARK+1:MARK+D]
+
+    MARK += D
+
     W2_ = @view param[MARK+1:MARK+D*M]
     
     W2 = reshape(W2_, D, M)
@@ -38,29 +42,33 @@ function unpack(n::net, param)
 
     @assert(MARK == length(param))
 
-    return b1, W1, W2
+    return b1, W1, b2, W2
 
 end
 
 function (n::net)(param, X)
 
-    b1, W1, W2 = unpack(n, param)
+    b1, W1, b2, W2 = unpack(n, param)
 
-    α = tanh.(W1*X .+ b1) # W1 is M×Q, X is Q×N
+    # W1 is M×Q, X is Q×N
 
-    return n.o(W2*α)      # activations are M×N
+    α = tanh.(W1*X .+ b1) # activations are M×N
+
+    return n.o(W2*α .+ b2)
 
 end
 
 function (n::net)(param)
 
-    b1, W1, W2 = unpack(n, param)
+    b1, W1, b2, W2 = unpack(n, param)
 
     function pred(X)
 
-        α = tanh.(W1*X .+ b1)  # W1 is M×Q, X is Q×N
+        # W1 is M×Q, X is Q×N
 
-        return n.o(W2*α)       # activations are M×N
+        α = tanh.(W1*X .+ b1) # activations are M×N
+
+        return n.o(W2*α .+ b2)
 
     end
 
@@ -68,5 +76,5 @@ end
 
 
 function Base.show(io::IO, n::net)
-    print(io, "net with ",n.Q ," inputs, ",n.M," hidden units and ",n.D , " outputs.\n Number of weights is ",numparam(n),". \n")
+    print(io, "net with ",n.Q ," inputs, ",n.M," hidden units and ",n.D , " outputs.\n Activation function is ",n.h, ". \n Output function is ",n.o)
 end
